ee24cc31f64279bc7399201fdfd8edf00e9367f1
AlexandruPosa <alex.posa94@yahoo.com>
2022-04-07 22:46:42 +0300

    Updates

---
+++ precache-manifest.219e936a5f49fb8e07043c8a25ca4bca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6767a55b2e9133aae7eb61527c371feb",
+    "url": "/movies-project/index.html"
+  },
+  {
+    "revision": "a05fb1b9265c95437175",
+    "url": "/movies-project/static/css/2.4925611d.chunk.css"
+  },
+  {
+    "revision": "d9758f1769293837ae17",
+    "url": "/movies-project/static/css/main.4bd52ffb.chunk.css"
+  },
+  {
+    "revision": "a05fb1b9265c95437175",
+    "url": "/movies-project/static/js/2.15c4c717.chunk.js"
+  },
+  {
+    "revision": "219dacb64eaa19765b25841ea9342277",
+    "url": "/movies-project/static/js/2.15c4c717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9758f1769293837ae17",
+    "url": "/movies-project/static/js/main.38eb4289.chunk.js"
+  },
+  {
+    "revision": "c856c5b7d481b1ddb708",
+    "url": "/movies-project/static/js/runtime-main.206eb8a6.js"
+  },
+  {
+    "revision": "dd8d447d593e8b863373d8a005ea1140",
+    "url": "/movies-project/static/media/contact-form-background.dd8d447d.jpg"
+  },
+  {
+    "revision": "ed0a27e86ad7344ee0fdfbb53d092d12",
+    "url": "/movies-project/static/media/mr3.ed0a27e8.PNG"
+  }
+]);
